6dd1310bbb4452c82d684769e693dc31a2074bbf
Eryn <johnmary1010@gmail.com>
2019-05-14 18:40:54 -0500

    update

--- server.js
+++ server.js
@@ -22,15 +22,13 @@ mongoose.connect(mongo_uri, { useNewUrlParser: true }, function(err) {
   }
 });
 if(process.env.NODE_ENV == 'production') {
-  app.use('/', express.static(path.join(__dirname, '/client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    app.use('/',router);
-  });
-} else {
-  app.use(express.static(path.join(__dirname, 'public')));
-}
-
+  app.use('/', express.static('/client/build'));
+  // app.get('*', (req, res) => {
+    // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    // });
+  } else {
+    app.use(express.static(path.join(__dirname, 'public')));
+  }
 
 app.use('/',router);
 

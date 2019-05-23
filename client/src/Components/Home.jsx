4a1312a8c0d29197ae4a6e738672380890743528
Eryn <johnmary1010@gmail.com>
2019-05-23 13:47:06 -0500

    update

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import Navigation from '../Navigation';
+import { Carousel } from 'react-bootstrap';
+import '../img/mirror.jpg';
+import '../img/map.JPG';
+import '../img/forecast.JPG';
 export default class Home extends Component {
 
   constructor(props) {
@@ -15,6 +19,43 @@ export default class Home extends Component {
     return (
       <>
       <Navigation />
+      <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="../img/mirror.jpg"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="../img/mirror.jpg"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="../img/mirror.jpg"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
     
 
 
